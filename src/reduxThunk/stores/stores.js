b6fb381ededc13e655379e17705dd7859acd0316
jessi68 <lynn312@unist.ac.kr>
2021-11-05 14:19:19 +0900

    config, feat: 가게 정보 가져오는 기능 구현, firebase 설치함

---
+++ src/reduxThunk/stores/stores.js
@@ -0,0 +1,38 @@
+import { firestore } from "firebase-admin";
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDoYZ6xSDj42gUFqLNwByd-2Xl0LgQkDsQ",
+  authDomain: "reservation-7cc8e.firebaseapp.com",
+  projectId: "reservation-7cc8e",
+  storageBucket: "reservation-7cc8e.appspot.com",
+  messagingSenderId: "517696782099",
+  appId: "1:517696782099:web:6acf834ab351927972a4e6",
+  measurementId: "G-JRV9XZKHTW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const fireStore = firebase.firestore();
+
+export default firebase;
+
+export const fetchStores = async () => {
+    let stores = [];
+    const documents = await fireStore.collection("Store").get();
+    documents.forEach(querySnapShot => {
+        console.log(querySnapShot.data());
+        stores.push({id: querySnapShot.id, 
+        ...(querySnapShot.data())})
+
+    }
+        );
+        
+        console.log(stores);
+    return stores;
+}
