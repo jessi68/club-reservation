23979df5ee4c383a23c8714604933ebeaa846d2d
jessi68 <lynn312@unist.ac.kr>
2021-11-12 00:24:53 +0900

    feat: 3km 내에 가까운 매장이 없는 경우 매장이 없다는 경고 표시 기능 구현

--- src/components/stores/stores.jsx
+++ src/components/stores/stores.jsx
@@ -6,7 +6,8 @@ export default  function Stores({isLoaded, stores}) {
     return (
         <div className="container">
             {isLoaded && <h1>로딩중입니다.</h1>}
-            {!isLoaded && stores 
+            {!isLoaded && stores.length === 0 &&  <h1>3km 내에 매장이 없습니다.</h1>}
+            {!isLoaded && stores
             && stores.map((store) => <StoreDetail store={store} key={store.name}></StoreDetail>)}
         </div>
     );
